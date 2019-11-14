d4967bf279a14bb2f7829f245c1b60e68dcf2e02
Roman Misnikov <r.misnikov@cft.ru>
2019-11-14 17:25:31 +0300

    Создал начальный проект

---
+++ ReduxWeather/Screens/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  ReduxWeather
+//
+//  Created by Мисников Роман on 14/11/2019.
+//  Copyright © 2019 Мисников Роман. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {}
+
